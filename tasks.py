4e2ebb7944935129db1a988102482e35738642f8
dni ⚡ <office@dnilabs.com>
2025-02-26 12:56:31 +0100

    feat: pass previous memo through (#31)
    now the memo of the payment has this form
    `{memo;memo_parent_payment;payment_hash_parent_payment}

--- tasks.py
+++ tasks.py
@@ -46,7 +46,9 @@ async def on_invoice_paid(payment: Payment) -> None:
         if target.percent > 0:
             amount_msat = int(payment.amount * target.percent / 100)
             memo = (
-                f"Split payment: {target.percent}% for {target.alias or target.wallet}"
+                f"Split payment: {target.percent}% "
+                f"for {target.alias or target.wallet}"
+                f";{payment.memo};{payment.payment_hash}"
             )
 
             if "@" in target.wallet or "LNURL" in target.wallet:
